71748614802e24a8d103b146a58155442c76ccc3
firchun025 <firchun025@gmail.com>
2023-08-15 14:29:56 +0900

    change css

--- resources/views/layouts/component/head.blade.php
+++ resources/views/layouts/component/head.blade.php
@@ -18,7 +18,7 @@
         rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/sb-admin-2.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/sb-admin-2-2.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
     <!-- Favicon -->
