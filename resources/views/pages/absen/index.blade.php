8927b3058fa7a1ca6b22972b1b9c4695bb00e178
firchun025 <firchun025@gmail.com>
2023-08-19 19:17:22 +0900

    test coordinate and marker

--- resources/views/pages/absen/index.blade.php
+++ resources/views/pages/absen/index.blade.php
@@ -10,9 +10,9 @@
             @include('layouts.component.alert')
             @include('layouts.component.alert_validate')
             <div class="row">
-                <div class="col-12 text-center">
+                {{-- <div class="col-12 text-center">
                     <div id="coordinates"></div>
-                </div>
+                </div> --}}
                 {{-- {{ Crypt::encryptString('firman') }} --}}
                 <div class="col-md-4">
                     <div class="card shadow-sm">
@@ -23,8 +23,10 @@
                                 <h5>{{ __('Buat Absen') }}</h5>
                             </div>
                             <div class="card-body">
-                                <input type="text" id="latitude" name="latitude">
-                                <input type="text" id="longitude" name="longitude">
+                                <input type="hidden" id="latitude" name="latitude">
+                                <input type="hidden" id="longitude" name="longitude">
+                                <div id="map" style="height: 400px;" class="my-4"></div>
+                                <div id="coordinates" class="mb-3"></div>
                                 <div class="form-group">
                                     <label for="day">Pilih Jadwal</label>
                                     <select class="form-control" name="id_jadwal">
@@ -46,6 +48,7 @@
                             </div>
                         </form>
                     </div>
+
                 </div>
                 <div class="col-md-8">
                     <div class="card shadow-sm">
@@ -110,6 +113,10 @@
     </div>
 @endsection
 @push('js')
+    {{-- <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap" async defer></script> --}}
+    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+        integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY=" crossorigin="" />
+    <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
     <script>
         document.addEventListener("DOMContentLoaded", function() {
             getLocation();
@@ -129,8 +136,8 @@
         }
 
         function showPosition(position) {
-            var latitude = position.coords.latitude.toFixed(3);
-            var longitude = position.coords.longitude.toFixed(3);
+            var latitude = position.coords.latitude.toFixed(4);
+            var longitude = position.coords.longitude.toFixed(4);
 
             // Tampilkan koordinat
             var coordinatesElement = document.getElementById("coordinates");
@@ -139,7 +146,28 @@
             // Isi nilai input dengan koordinat
             document.getElementById("latitude").value = latitude;
             document.getElementById("longitude").value = longitude;
+            initMap(latitude, longitude);
         }
+
+        function initMap(latitude, longitude) {
+            var map = L.map('map').setView([latitude, longitude], 20);
+
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+            }).addTo(map);
+
+            L.marker([latitude, longitude]).addTo(map)
+                .bindPopup('Lokasi Anda')
+                .openPopup();
+
+            var circle = L.circle([latitude, longitude], {
+                color: 'red',
+                fillColor: '#f03',
+                fillOpacity: 0.5,
+                radius: 15
+            }).addTo(map);
+        }
+
 
         function showError(error) {
             switch (error.code) {
